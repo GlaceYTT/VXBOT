45aa7569654f2d76f089a9af344a6e0587f2725b
GlaceYTT <official.glaceyt@gmail.com>
2024-12-08 13:03:48 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -9,19 +9,21 @@ const transactionSchema = new mongoose.Schema({
     ownerName: String,
     country: String,
     receiveCountry: String,
-    currency: String,
+    sendCurrency: { type: String }, 
+    receiveCurrency: { type: String },
     sendMethod: String,
     receiveMethod: String,
     amount: Number,
     paymentMethod: String,
     transactionId: String,
     status: String,
-    type: { type: String, enum: ['Currency', 'Crypto'] }, // Make it optional
-    cryptoType: { type: String }, // Type of cryptocurrency if 'Crypto'
-    sendNetwork: { type: String }, // Network for sending, e.g., TRC-20/Bitcoin
-    receiveNetwork: { type: String }, // Network for receiving
-    sendingDetails: { type: Object, default: {} }, // Separate field for sending details
-    receivingDetails: { type: Object, default: {} }, // Separate field for receiving details
+    type: { type: String, enum: ['Currency', 'Crypto'] }, 
+    sendingCryptoType: { type: String },
+    receivingCryptoType: { type: String },
+    sendNetwork: { type: String }, 
+    receiveNetwork: { type: String }, 
+    sendingDetails: { type: Object, default: {} }, 
+    receivingDetails: { type: Object, default: {} }, 
     userType: { type: String, enum: ['Free', 'Premium'], default: 'Free' },
     createdAt: { type: Date, default: Date.now },
 });
